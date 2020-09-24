90f7e3a3f1a77a40c685d1ca210e4f755c7428b8
Andrei Kovaci <andrei.kovaci@gmail.com>
2020-09-24 17:45:37 +0100

    finishing the last lectures in section 5

---
+++ project/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\BlogPost;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $blogPost = new BlogPost();
+        $blogPost->setTitle('A first post!');
+        $blogPost->setPublished(new \DateTime('2020-09-24 12:00:00'));
+        $blogPost->setContent('Post Text!');
+        $blogPost->setAuthor('General Kenobi');
+        $blogPost->setSlug('a-first-post');
+
+        $manager->persist($blogPost);
+
+        $blogPost = new BlogPost();
+        $blogPost->setTitle('A second post!');
+        $blogPost->setPublished(new \DateTime('2020-09-24 12:00:00'));
+        $blogPost->setContent('Second Post Text!');
+        $blogPost->setAuthor('General Kenobi');
+        $blogPost->setSlug('a-second-post');
+
+        $manager->persist($blogPost);
+
+        $manager->flush();
+    }
+}
