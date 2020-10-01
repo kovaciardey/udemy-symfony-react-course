b0c3f50420f557e8674fc0edef18e4f0a0ac67c0
Andrei Kovaci <andrei.kovaci@gmail.com>
2020-10-01 19:29:00 +0100

    Update the fixtures tu add a relation between a user and a comment

--- project/src/DataFixtures/AppFixtures.php
+++ project/src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\BlogPost;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,11 +11,19 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $this->loadUsers($manager);
+        $this->loadBlogPosts($manager);
+    }
+
+    public function loadBlogPosts(ObjectManager $manager)
+    {
+        $user = $this->getReference('user_admin');
+
         $blogPost = new BlogPost();
         $blogPost->setTitle('A first post!');
         $blogPost->setPublished(new \DateTime('2020-09-24 12:00:00'));
         $blogPost->setContent('Post Text!');
-        $blogPost->setAuthor('General Kenobi');
+        $blogPost->setAuthor($user);
         $blogPost->setSlug('a-first-post');
 
         $manager->persist($blogPost);
@@ -23,11 +32,31 @@ class AppFixtures extends Fixture
         $blogPost->setTitle('A second post!');
         $blogPost->setPublished(new \DateTime('2020-09-24 12:00:00'));
         $blogPost->setContent('Second Post Text!');
-        $blogPost->setAuthor('General Kenobi');
+        $blogPost->setAuthor($user);
         $blogPost->setSlug('a-second-post');
 
         $manager->persist($blogPost);
 
+        $manager->flush();
+    }
+
+    public function loadComments(ObjectManager $manager)
+    {
+
+    }
+
+    public function loadUsers(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setEmail('admin@blog.com');
+        $user->setName('General Kenobi');
+
+        $user->setPassword('secret123#');
+
+        $this->addReference('user_admin', $user);
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
