d66f504833340a7cb5cf5a5a3d923d3141611ea1
Andrei Kovaci <andrei.kovaci@gmail.com>
2021-10-14 16:57:49 +0100

    added special group for the comments subresource to get the user details when getting the details of a comment

--- project/src/Entity/Comment.php
+++ project/src/Entity/Comment.php
@@ -25,7 +25,14 @@ use Symfony\Component\Validator\Constraints as Assert;
  *      },
  *     denormalizationContext={
  *          "groups"={"post"}
- *      }
+ *      },
+ *     subresourceOperations={
+ *          "api_blog_posts_comments_get_subresource"={
+ *              "normalization_context"={
+ *                  "groups"={"get-comment-with-author"}
+ *              }
+ *          }
+ *     }
  * )
  * @ORM\Entity(repositoryClass=CommentRepository::class)
  */
@@ -35,12 +42,13 @@ class Comment implements AuthoredEntityInterface, PublishedDateEntityInterface
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"get-comment-with-author"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="text")
-     * @Groups({"post"})
+     * @Groups({"post", "get-comment-with-author"})
      * @Assert\NotBlank()
      * @Assert\Length(min=5, max=3000)
      */
@@ -48,12 +56,14 @@ class Comment implements AuthoredEntityInterface, PublishedDateEntityInterface
 
     /**
      * @ORM\Column(type="datetime")
+     * @Groups({"get-comment-with-author"})
      */
     private $published;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="comments")
      * @ORM\JoinColumn(nullable=false)
+     * @Groups({"get-comment-with-author"})
      */
     private $author;
 
